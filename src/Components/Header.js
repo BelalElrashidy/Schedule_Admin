83fa8131b8493dd6343b5bbb4ab47b577dabf946
BeboElRashidy <bebo.elrashidy22@gmail.com>
2023-07-02 15:48:02 +0300

    Editing

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,17 @@
-import '../assets/css/main.css';
-function Header(){
-    return(
-        <>
-            <header>
-                <nav className="nav">
-                    <div className="bar">
-                        <p>
-                            Admin
-                        </p>
-                        <a>Logout</a>
-                    </div>
-                </nav>
-            </header>
-        </>
-    )
+import "../assets/css/main.css";
+function Header() {
+  return (
+    <>
+      <header>
+        <nav className="nav">
+          <div className="bar">
+            <p>Admin</p>
+            <button className="log"> logout </button>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
 }
 
 export default Header;
--- src/Components/ScheduleComponent.js
+++ src/Components/ScheduleComponent.js
@@ -1,14 +1,15 @@
-import '../assets/css/main.css';
+import "../assets/css/main.css";
+let comp = ["time"];
 
-
-function Components(){
-    return(
-        <>
-            {comp.map((item) => (
-                <li key={item} className='component'>
-                    {item}
-                </li>
-            ))}
-        </>
-    );
+function Components() {
+  return (
+    <>
+      {comp.map((item) => (
+        <li key={item} className="component">
+          {item}
+        </li>
+      ))}
+    </>
+  );
 }
+export default Components;
